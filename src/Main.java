beb4575f6c0a5f9b89c7b3057cc268d4614a316c
“Sergei <sergei.kudris@gmail.com>
2024-10-16 22:48:46 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 30202;
+        int CostOfMiles = 20;
+        int bonus = amount / CostOfMiles;
+        System.out.println("Начисленные мили с покупки билета: " + bonus);
+    }
+}
